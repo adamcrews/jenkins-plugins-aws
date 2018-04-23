2c61528686a5554c0a47b10d4860f7612d04ca7e
Adam Crews <adam.crews@gmail.com>
2018-04-23 15:31:56 -0700

    WIP - refactor, getting code almost working

--- iam.tf
+++ iam.tf
@@ -30,16 +30,44 @@ resource "aws_iam_role_policy" "jenkins-plugins-sqs-policy" {
 {
   "Version": "2012-10-17",
   "Statement": [
-    { 
-      "Effect": "Allow", 
-      "Action": [ 
-        "sqs:GetQueueAttributes", 
-        "sqs:GetQueueUrl", 
-        "sqs:SendMessage*"
-      ], 
-      "Resource": [ 
-        "${aws_sqs_queue.jenkins_plugins.arn}" 
-      ] 
+    {
+      "Effect": "Allow",
+      "Action": [
+        "sqs:GetQueueAttributes",
+        "sqs:GetQueueUrl",
+        "sqs:SendMessage*",
+        "sqs:ReceiveMessage",
+        "sqs:ChangeMessageVisibility",
+        "sqs:DeleteMessage"
+      ],
+      "Resource": [
+        "${aws_sqs_queue.jenkins-plugins.arn}"
+      ]
+    }
+  ]
+}
+EOF
+}
+
+# This is added to the lambda role to allow access to post sqs messages
+resource "aws_iam_role_policy" "jenkins-plugins-lambda-policy" {
+  name = "jenkins-plugins-lambda-policy"
+  role = "${aws_iam_role.jenkins-plugins-role.id}"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "lambda:InvokeFunction"
+      ],
+      "Resource": [
+        "${aws_lambda_function.jenkins-plugins-get-plugins.arn}",
+        "${aws_lambda_function.jenkins-plugins-create-packages.arn}",
+        "${aws_lambda_function.jenkins-plugins-dispatcher.arn}"
+      ]
     }
   ]
 }
--- src/dispatcher.js
+++ src/dispatcher.js
@@ -1,12 +1,12 @@
 "use strict";
 
 // Load Required libraries
-const AWS = require("aws-sdk");
-const async = require("async");
+const AWS    = require("aws-sdk");
+const async  = require("async");
 const config = require("./config");
 
 // Set the region... is this really needed?
-AWS.config.update({ region: process.env.region });
+AWS.config.update({ region: config.REGION });
 
 // Create the sqs service opbject
 var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
@@ -15,7 +15,7 @@ var lambda = new AWS.Lambda();
 function receiveMessages(callback) {
   var params = {
     QueueUrl: config.SQS_QUEUE,
-    MaxNumberOfMessages: 100
+    MaxNumberOfMessages: 10
   };
   sqs.receiveMessage(params, function(err, data) {
     if (err) {
@@ -29,7 +29,7 @@ function receiveMessages(callback) {
 
 function invokeWorkerLambda(task, callback) {
   var params = {
-    FunctionName: "jenkins-plugins-rpm-package",
+    FunctionName: "jenkins-plugins-create-packages",
     InvocationType: "Event",
     Payload: JSON.stringify(task)
   };
