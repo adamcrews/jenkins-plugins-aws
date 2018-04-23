5bc3f260f8e1689080bf87f52b2282e69bf48a0e
Adam Crews <adamc@slalom.com>
2018-04-23 15:31:56 -0700

    WIP - refactor, getting code almost working

--- src/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const AWS = require("aws-sdk");
-
-// Set the region... is this really needed?
-AWS.config.update({ region: process.env.region });
-
-// Is the version needed?
-var sqs = new AWS.SQS({ apiVersion: "2012-11-05" });
--- src/package.js
+++ src/package.js
@@ -1,51 +1,53 @@
 'use strict';
 
 // Load Required libraries
-const https = require('https');
 const AWS   = require('aws-sdk');
+
 // import the config module
 const config = require('./config');
 
 // Set the region... is this really needed?
-AWS.config.update({region: process.env.region});
+AWS.config.update({ region: config.REGION });
 
 // Create the sqs service opbject
-var sqs = new AWS.SQS({apiVersion: '2012-11-05'});
+var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
 var s3  = new AWS.S3();
 
 function deleteMessage(receiptHandle, cb) {
   sqs.deleteMessage({
     ReceiptHandle: receiptHandle,
-    // reference the SQS_QUEUE param on the imported config object
     QueueUrl: config.SQS_QUEUE
   }, cb);
 }
 
 function work(task, cb) {
-  var message = JSON.parse(task);
-  //console.log(task);
-  pName = message.name;
+  let message = JSON.parse(task);
+  console.log(message);
+
+  let pName = message.name;
   console.log(pName);
 
-  let params = {
-    Bucket: config.S3_BUCKET,
-    Key: '/rpm/' + pName + '.rpm'
-  };
+  // let params = {
+  //  Bucket: config.S3_BUCKET,
+  //  Key: '/rpm/' + pName + '.rpm'
+  // };
 
-  s3.headObject(params, function (err, metadata) {
-    if (err && err.code === 'NotFound') {
+  //s3.headObject(params, function (err, metadata) {
+  //  if (err && err.code === 'NotFound') {
     // stuff
-    }
-  });
+  //  console.log('No s3 item');
+  //  }
+  //});
   cb();
 }
 
-exports.build_rpm = function(event, context, callback) {
+exports.handler = function(event, context, callback) {
   work(event.Body, function(err) {
-    if (err);
+    if (err) {
       callback(err);
     } else {
-      deleteMessage(event.ReceiptHandle, callback);
+      //deleteMessage(event.ReceiptHandle, callback);
+      console.log(event.ReceiptHandle);
     }
   });
 };
