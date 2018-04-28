10ca4608f3aaf0ef6d86ee87420990699d4eb7ec
Adam Crews <adamc@slalom.com>
2018-04-28 08:05:21 -0700

    Working build

--- src/config.js
+++ src/config.js
@@ -1,7 +1,10 @@
 // define and export all your common constants here
 module.exports = {
+  REGION: process.env.AWS_REGION,
   SQS_QUEUE: process.env.sqsQueue,
+  SNS_TOPIC: process.env.snsTopic,
   S3_BUCKET: process.env.s3Bucket,
   RELEASE: process.env.release,
-  PLUGIN_URL: "https://updates.jenkins-ci.org/current/update-center.actual.json"
+  PLUGIN_URL: "https://updates.jenkins-ci.org/current/update-center.actual.json",
+  RPMROOT: "/tmp/rpmbuild"
 };
--- src/package.js
+++ src/package.js
@@ -2,15 +2,14 @@
 
 // Load Required libraries
 const AWS   = require('aws-sdk');
-const fs    = require('fs');
+const fs    = require('fs-extra');
 const path  = require('path');
 const { exec }  = require('child_process');
 
 // import the config module
 const config = require('./config');
 
-// Set the region... is this really needed?
-AWS.config.update({ region: process.env.AWS_REGION });
+AWS.config.update({ region: config.AWS_REGION });
 
 // Create the sqs service opbject
 var sqs = new AWS.SQS({ apiVersion: '2012-11-05' });
@@ -31,6 +30,37 @@ function strip_extension(str) {
   return str.substr(0,str.lastIndexOf('.'));
 }
 
+function uploadFile(bucket, fileName, data) {
+  let params = {
+    Bucket: bucket,
+    Key: fileName,
+    Body: data
+  };
+
+  s3.upload(params, function(err, data) {
+    if (err) {
+      console.log(err);
+      throw err;
+    }
+  });
+}
+
+function checkFile(bucket, filename) {
+  return new Promise(function(resolve, reject) {
+    let params = {
+      Bucket: bucket,
+      Key: filename
+    };
+
+    s3.headObject(params, function (err, metadata) {
+      if (err && err.code === 'NotFound') {
+        reject("NotFound");
+      }
+      resolve("Found");
+    });
+  });
+}
+
 function mkDirByPathSync(targetDir, {isRelativeToScript = false} = {}) {
   const sep     = path.sep;
   const initDir = path.isAbsolute(targetDir) ? sep : '';
@@ -40,7 +70,7 @@ function mkDirByPathSync(targetDir, {isRelativeToScript = false} = {}) {
     const curDir = path.resolve(baseDir, parentDir, childDir);
     try {
       fs.mkdirSync(curDir);
-      console.log(`Directory ${curDir} created!`);
+      //console.log(`Directory ${curDir} created!`);
     } catch (err) {
       if (err.code !== 'EEXIST') {
         throw err;
@@ -52,23 +82,64 @@ function mkDirByPathSync(targetDir, {isRelativeToScript = false} = {}) {
   }, initDir);
 }
 
+function makeBuildDirs(root, subDirs) {
+  return new Promise(resolve => {
+    // make sure we have a clean tmp area for the packages.  We might get the
+    // same container as the previous invocation, and don't want leftovers from a
+    // previous build.
+    fs.removeSync(root);
+    for (var d in subDirs) {
+      mkDirByPathSync(root + '/' + subDirs[d]);
+    }
+  });
+}
+
+function buildRpm(cmd, rpmName, srcrpmName) {
+  return new Promise(function(resolve, reject) {
+    exec(cmd, (err, stdout, stderr) => {
+      if (err) {
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+        reject(err);
+      }
+
+      let data = {
+        rpm:  fs.readFileSync(config.RPMROOT + '/RPMS/noarch/' + rpmName, 'utf8'),
+        srpm: fs.readFileSync(config.RPMROOT + '/SRPMS/' + srcrpmName, 'utf8')
+      };
+
+      resolve(data);
+    });
+  });
+}
+
 function work(task, cb) {
-  const mustache = require('mustache');
-  const buildRoot  = '/tmp/rpmbuild';
-
   var message = JSON.parse(task);
-  let templ   = fs.readFileSync(process.cwd() + '/rpm_spec.mustache', 'utf8');
-
   message.release          = config.RELEASE;
   message.version_underbar = message.version.replace(/-/g,"_");
   message.filename         = strip_extension(basename(message.url,'/'));
 
-  let buildDirs = [ 'BUILD', 'BUILDROOT', 'RPMS', 'SPECS', 'SRPMS', 'SOURCES', 'tmp' ];
-  for (var d in buildDirs) {
-    mkDirByPathSync(buildRoot + '/' + buildDirs[d]);
-  }
+  let rpmName = 'jenkins-plugin-' + message.name + '-' + message.version_underbar + '-' + message.release + '.noarch.rpm';
+  let srcrpmName = 'jenkins-plugin-' + message.name + '-' + message.version_underbar + '-' + message.release + '.src.rpm';
 
-  let specFile = buildRoot + '/SPECS/jenkins-plugin-' + message.name + '.spec';
+  // if a file exists, we just bail
+  checkFile(config.S3_BUCKET, 'rpms/' + rpmName)
+    .then(() => {
+      console.log('Package already present: Skipping.');
+      process.exit();
+      })
+    .catch(() => {
+      return true;
+  });
+
+  const mustache = require('mustache');
+
+  let templ   = fs.readFileSync(process.cwd() + '/rpm_spec.mustache', 'utf8');
+
+  let buildDirs = [ 'BUILD', 'BUILDROOT', 'RPMS', 'SPECS', 'SRPMS', 'SOURCES', 'tmp' ];
+  makeBuildDirs(config.RPMROOT, buildDirs);
+
+  let specFile = config.RPMROOT + '/SPECS/jenkins-plugin-' + message.name + '.spec';
   fs.writeFile(
     specFile,
     mustache.render(templ, message),
@@ -76,30 +147,13 @@ function work(task, cb) {
       if(err) throw err;
   });
 
-  let buildCmd = process.cwd() + '/rpmbuild --define \'_tmppath ' + buildRoot + '/tmp\' --define \'_topdir ' + buildRoot + '\' --buildroot ' + buildRoot + ' -ba ' + specFile;
-  exec(buildCmd, (err, stdout, stderr) => {
-    if (err) {
-      // node couldn't execute the command
-      console.log(err);
-      return;
-    }
-
-    // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
+  let buildCmd = process.cwd() + '/rpmbuild --define \'_tmppath ' + config.RPMROOT + '/tmp\' --define \'_topdir ' + config.RPMROOT + '\' --buildroot ' + config.RPMROOT + '/BUILDROOT -ba ' + specFile;
+  buildRpm(buildCmd, rpmName, srcrpmName).then(function (data) {
+    console.log(config.S3_BUCKET, 'rpms/'+ rpmName);
+    uploadFile(config.S3_BUCKET, 'rpms/'+ rpmName, data.rpm);
+    uploadFile(config.S3_BUCKET, 'src/' + srcrpmName, data.srpm);
   });
 
-  // let params = {
-  //  Bucket: config.S3_BUCKET,
-  //  Key: '/rpm/' + pName + '.rpm'
-  // };
-
-  //s3.headObject(params, function (err, metadata) {
-  //  if (err && err.code === 'NotFound') {
-    // stuff
-  //  console.log('No s3 item');
-  //  }
-  //});
   cb();
 }
 
@@ -108,8 +162,8 @@ exports.handler = function(event, context, callback) {
     if (err) {
       callback(err);
     } else {
-      //deleteMessage(event.ReceiptHandle, callback);
-      console.log(event.ReceiptHandle);
+      deleteMessage(event.ReceiptHandle, callback);
+      //console.log(event.ReceiptHandle);
     }
   });
 };
